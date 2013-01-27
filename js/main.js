3703d92b03b60995e3cadf283d1582fc8a08419c
Alice <blcuydy+git@gmail.com>
2013-01-27 14:27:00 +0800

    make a circle appear\!

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var svg = d3.select('#game').append('svg')
+	.attr('width', 500)
+	.attr('height', 500)
+var circle = svg.append('circle')
+
+circle.attr('cx', 100)
+circle.attr('cy', 200)
+circle.attr('r', 30)
+
