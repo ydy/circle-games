8f5d42a2d7437c09e197a906a2abd8726462eb7b
Alice <blcuydy+git@gmail.com>
2013-02-06 15:28:31 +0800

    animate the circles

--- js/main.js
+++ js/main.js
@@ -1,33 +1,55 @@
-var circles = []
+
+var maxHeight = 300
+var maxWidth = 500
+var svg = d3.select('#game').append('svg')
+  .attr('width', maxWidth)
+  .attr('height', maxHeight)
 
 randomNumber = function(lower, upper){
   return Math.floor(Math.random() * (upper-lower+1) + lower)
 }
 
 randomCircle = function(){
-  return {cx:randomNumber(0, 500), cy:randomNumber(0,500), r:randomNumber(5,20)}
+  return {cx:randomNumber(0, maxWidth), cy:randomNumber(0, maxHeight), r:randomNumber(5,20)}
 }
 
+function makeCircles() {
+  var circles = []
+  var circlesNumber = randomNumber(5, 50)
 
-
-var circlesNumber = 10
-
-for(var i = 0; i < 10; i++){
+  for(var i = 0; i < circlesNumber; i++){
    circles[i] = randomCircle()
+  }
+  return circles
 }
 
-var svg = d3.select('#game').append('svg')
-	.attr('width', 500)
-	.attr('height', 500)
-var piecesEnter = svg.selectAll('.pieces').data(circles).enter().append('circle')
-piecesEnter.classed('pieces', true)
-piecesEnter.attr('cx', function(c){return c.cx})
-piecesEnter.attr('cy', function(c){return c.cy})
-piecesEnter.attr('r', function(c){return c.r})
+function drawCircles() {
+  var allCircles = svg.selectAll('.pieces').data(makeCircles())
+  allCircles.transition().duration(1000)
+    .attr('cx', function(c){return c.cx})
+    .attr('cy', function(c){return c.cy})
+    .attr('r', function(c) {return c.r})
 
+  var piecesEnter = allCircles.enter().append('circle')
+  piecesEnter.classed('pieces', true)
+  //piecesEnter.attr('r', function(c){return c.r})
+  piecesEnter.attr('r', 0)
+  piecesEnter.attr('cx', function(c){return c.cx})
+  piecesEnter.attr('cy', function(c){return c.cy})
+  piecesEnter.transition().duration(1000).attr('r', function(c) {
+    return c.r
+  })
+  
+  allCircles.exit().transition().duration(1000).attr('r', 0).remove()
 
-
-
-
-
-
+  //allCircles.attr('r', function(c){return c.r})
+}
+setInterval(drawCircles, 1000)
+drawCircles()
+/*
+piecesEnter.transition().duration(function(c, i){
+  return 200*i
+}).attr('cy', function(c) {
+  return c.r
+})
+*/
