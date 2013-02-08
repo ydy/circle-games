799cac1faeb79403f67a82fb699945aba05e01a7
Alice <blcuydy+git@gmail.com>
2013-02-08 21:11:48 +0800

    circle game with setup position

--- js/main.js
+++ js/main.js
@@ -5,11 +5,10 @@ var svg = d3.select('#game').append('svg')
   .attr('width', maxWidth)
   .attr('height', maxHeight)
 
-randomNumber = function(lower, upper){
+function randomNumber(lower, upper){
   return Math.floor(Math.random() * (upper-lower+1) + lower)
 }
-
-randomCircle = function(){
+function randomCircle(){
   return {cx:randomNumber(0, maxWidth), cy:randomNumber(0, maxHeight), r:randomNumber(5,20)}
 }
 
@@ -22,6 +21,8 @@ function makeCircles() {
   }
   return circles
 }
+
+var counter = 0
 
 function drawCircles() {
   var allCircles = svg.selectAll('circle.pieces').data(makeCircles())
@@ -38,8 +39,35 @@ function drawCircles() {
   piecesEnter.transition().duration(1000).attr('r', function(c) {
     return c.r
   })
+  piecesEnter.on('click', function(circle){
+    d3.select(this).transition().duration(1000).style('fill','blue')
+    counter += 21-circle.r
+    drawScore()
+  })
   
   allCircles.exit().transition().duration(1000).attr('r', 0).remove()
 }
-setInterval(drawCircles, 1000)
-drawCircles()
+
+
+function removeCircles(){
+  svg.selectAll('circle').transition().duration(1000).attr('r', 0).remove()
+}
+
+function drawScore(){
+  d3.select('#score').text('Score: '+ counter)
+}
+
+d3.select('#start').on('click', function(){
+  counter = 0  
+  drawScore()
+  var drawTimer = setInterval(drawCircles, 1500)
+  drawCircles()
+  d3.select('#gameover').classed('finish', false)
+  setTimeout(function gameover() {
+    clearInterval(drawTimer)
+    removeCircles()
+    d3.select('#gameover').classed('finish', true)
+  }, 1000)
+})
+
+
