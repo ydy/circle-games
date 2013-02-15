1b557974057575e45004bbc2a8bff2e35e072542
Alice <blcuydy+git@gmail.com>
2013-02-15 11:42:06 +0800

    classic example

--- js/main.js
+++ js/main.js
@@ -22,30 +22,43 @@ function makeCircles() {
   return circles
 }
 
+
+function makeNewCircle(c, angle) {
+  return {cx: c.cx + n*c.r*Math.cos(angle), cy: c.cy + n*c.r*Math.sin(angle), r: m*c.r}
+}
+
+function makeCircleExplosion(c) {
+  var circles = [c];
+  for (var i=0; i<8; i++) {
+    circles[i + 1] = makeNewCircle(c, i * Math.PI/4)
+  }
+  return circles
+}
+
 var counter = 0
 
-function drawCircles() {
-  var allCircles = svg.selectAll('circle.pieces').data(makeCircles())
+function drawCircles(circles) {
+  var allCircles = svg.selectAll('circle.pieces').data(circles)
   allCircles.transition().duration(1000)
     .attr('cx', function(c){return c.cx})
     .attr('cy', function(c){return c.cy})
     .attr('r', function(c) {return c.r})
 
-  var piecesEnter = allCircles.enter().append('circle')
-  piecesEnter.classed('pieces', true)
-  piecesEnter.attr('r', 0)
-  piecesEnter.attr('cx', function(c){return c.cx})
-  piecesEnter.attr('cy', function(c){return c.cy})
-  piecesEnter.transition().duration(1000).attr('r', function(c) {
-    return c.r
-  })
-  piecesEnter.on('click', function(circle){
-    d3.select(this).transition().duration(1000).style('fill','blue')
-    counter += 21-circle.r
-    drawScore()
-  })
-  
-  allCircles.exit().transition().duration(1000).attr('r', 0).remove()
+  allCircles.enter().append('circle')
+    .classed('pieces', true)
+    .attr('r', 0)
+    .attr('cx', function(c){return c.cx})
+    .attr('cy', function(c){return c.cy})    
+    .on('click', function(circle){
+      d3.select(this).transition().duration(1000).style('fill','blue')
+      counter += 21-circle.r
+      drawScore()
+    })
+    .transition()
+      .duration(1000).attr('r', function(c) {
+        return c.r
+      })
+    allCircles.exit().transition().duration(1000).attr('r', 0).remove()
 }
 
 
@@ -57,17 +70,58 @@ function drawScore(){
   d3.select('#score').text('Score: '+ counter)
 }
 
-d3.select('#start').on('click', function(){
-  counter = 0  
-  drawScore()
-  var drawTimer = setInterval(drawCircles, 1500)
-  drawCircles()
-  d3.select('#gameover').classed('finish', false)
-  setTimeout(function gameover() {
-    clearInterval(drawTimer)
-    removeCircles()
-    d3.select('#gameover').classed('finish', true)
-  }, 1000)
-})
+function playGame() {
+  function drawGameCircles() {
+    drawCircles(makeCircles())
+  }
+  d3.select('#start').on('click', function startGame(){
+    counter = 0  
+    drawScore()
+    var drawTimer = setInterval(drawGameCircles, 1500)
+    drawGameCircles()
+    d3.select('#gameover').classed('finish', false)
+    setTimeout(function gameOver() {
+      clearInterval(drawTimer)
+      removeCircles()
+      d3.select('#gameover').classed('finish', true)
+    }, 1000)
+  })
+}
+
+playGame()
+var n = 2.4
+var m = 0.28
+var circleList = makeCircleExplosion({ cx: 200, cy: 150, r: 50 })
+function makeSuperExplosion(circleList){
+  var manyCircles = circleList.map(function(circle) {
+    return makeCircleExplosion(circle)
+  }).reduce(function (circlesArr, circles) {
+    return circlesArr.concat(circles)
+  })
+  return manyCircles
+}
+drawCircles(makeSuperExplosion(circleList))
+/*
+1 array of 9 arrays of 9 circles
+1 array of 81 circles
+
+makeSuperExplosion(cicleList) {
+  var newArray = [];
+  circleList.forEach(function(circle, index) {
+     newArray[index] = makeCircleExplosion(circle)
+  });
+  return newArray.reduce(function (circlesArr, circles) {
+    return circlesArr.concat(circles)
+  })
+}
+*/
+
+
+
+
+
+
+
+
 
 
